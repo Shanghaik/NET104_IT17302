c9f141569b2f64e1aebcedff63f39aad135b3c32
shanghaik168@gmail.com <Shanghaik1998>
2023-03-16 09:14:22 +0700

    View-action

--- Shopping_Project/Controllers/HomeController.cs
+++ Shopping_Project/Controllers/HomeController.cs
@@ -15,10 +15,74 @@ namespace Shopping_Project.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); // Hiển thị View khi Action được gọi đến
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View(); // Gọi đến đúng View có tên như Action
+        }
+
+        public IActionResult Redirect()
+        {
+            // Một các câu lệnh xử lý nào đó mà không liên quan tới view
+            return RedirectToAction("Test"); // Chuyển hướng tói 1 Action khác
+        }
+
+        public IActionResult Show()
+        {
+            // Đẩy dữ liệu từ action qua view thông qua model object
+            // Có obj
+            Product product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Học lại", 
+                AvailableQuantity = 1,
+                Supplier = "MamaBank",
+                Price = 672000, 
+                Description = "Trượt điểm danh",
+                Status = new Random().Next(0, 100)
+            };
+            return View(product);  // Truyền thẳng vào view luôn
+            // 1 View truyền dc trực tiếp 1 Object model
+        }
+        public IActionResult ShowAllProducts()
+        {
+            // Đẩy dữ liệu từ action qua view thông qua model object
+            // Có obj
+            List<Product> products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Học lại",
+                    AvailableQuantity = 1,
+                    Supplier = "MamaBank",
+                    Price = 672000,
+                    Description = "Trượt điểm danh",
+                    Status = new Random().Next(0, 100)
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Vẫn là học lại",
+                    AvailableQuantity = 1,
+                    Supplier = "MamaBank",
+                    Price = 672000*4,
+                    Description = "Lập biên bản",
+                    Status = new Random().Next(0, 100)
+                },
+            };
+            return View(products);  // Truyền thẳng vào view luôn
+            // 1 View truyền dc trực tiếp 1 Object model
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
