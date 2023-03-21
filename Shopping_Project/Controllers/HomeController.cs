59c27aafaaebf02b7b059bf56a68e8e453648096
shanghaik168@gmail.com <Shanghaik1998>
2023-03-21 08:32:10 +0700

    Them-XOa

--- Shopping_Project/Controllers/HomeController.cs
+++ Shopping_Project/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Project.IServices;
 using Shopping_Project.Models;
+using Shopping_Project.Services;
 using System.Diagnostics;
 
 namespace Shopping_Project.Controllers
@@ -7,10 +9,11 @@ namespace Shopping_Project.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly IProductServices productServices; // interface
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            productServices = new ProductServices(); // Class
         }
 
         public IActionResult Index()
@@ -55,36 +58,32 @@ namespace Shopping_Project.Controllers
         {
             // Đẩy dữ liệu từ action qua view thông qua model object
             // Có obj
-            List<Product> products = new List<Product>()
-            {
-                new Product
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Học lại",
-                    AvailableQuantity = 1,
-                    Supplier = "MamaBank",
-                    Price = 672000,
-                    Description = "Trượt điểm danh",
-                    Status = new Random().Next(0, 100)
-                },
-                new Product
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Vẫn là học lại",
-                    AvailableQuantity = 1,
-                    Supplier = "MamaBank",
-                    Price = 672000*4,
-                    Description = "Lập biên bản",
-                    Status = new Random().Next(0, 100)
-                },
-            };
+            List<Product> products = productServices.GetAllProducts();
             return View(products);  // Truyền thẳng vào view luôn
             // 1 View truyền dc trực tiếp 1 Object model
         }
 
-        public IActionResult Create()
+        public IActionResult Create() // Hiển thị form ra cho người dùng thôi
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product) // Thực hiện thêm
+        {
+            if (productServices.CreateProduct(product))
+            {
+                return RedirectToAction("ShowAllProducts");
+            }
+            else return BadRequest();
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            if (productServices.DeleteProduct(id))
+            {
+                return RedirectToAction("ShowAllProducts");
+            }
+            else return BadRequest();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Shopping_Project/Program.cs
+++ Shopping_Project/Program.cs
@@ -1,9 +1,27 @@
+﻿using Shopping_Project.IServices;
+using Shopping_Project.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IProductServices, ProductServices>();
+// Có 3 hình thức áp dụng cho Dependency Injection
+/* 
+ * Add Transient: Khi có 1 request (yêu cầu mới). Mỗi yêu cầu HTTP request
+ * sẽ nhận một đối tượng services khác nhau, loại này phù hợp cho các services 
+ * mà không giữ trạng thái cố định và có tính chất phục vụ được cho nhiều loại
+ * reuqest.
+ * Add Scoped: Services tạo mới cho mỗi yêu cầu http request và sẽ giữ nguyên
+ * trong suốt quá trình xử lý yêu cầu đó. Phù hợp cho các services mà có thể
+ * giải quyết các yêu cầu http cụ thể nào đó.
+ * Add Singleton: Services chỉ được tạo 1 lần trong suốt vòng đời của ứng dụng
+ * các yêu cầu phía sau sẽ được nhận cùng 1 đối tượng services đó. Phù hợp cho 
+ * các services có tính toàn cục và không thay đổi thường xuyên.
+ */
 
-var app = builder.Build();
+
+var app = builder.Build(); //Build cuối cùng
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
