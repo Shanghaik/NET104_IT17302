e8e23391a6e10824bffa1a3443f845a7111282d7
shanghaik168@gmail.com <Shanghaik1998>
2023-03-30 08:23:29 +0700

    services

--- Shopping_Project/Controllers/HomeController.cs
+++ Shopping_Project/Controllers/HomeController.cs
@@ -135,7 +135,7 @@ namespace Shopping_Project.Controllers
             HttpContext.Session.SetString("thongbao", message);
             // Lấy data từ Session để sử dụng
             var data = HttpContext.Session.GetString("thongbao");
-            ViewData["message"] = data; 
+            ViewData["message"] = data;
             // Nguyên tắc tính thời gian timeout của Session
             // Khi chúng ta đã có dữ liệu trong Session thì bộ đếm thời gian 
             // sẽ được kích hoạt ngay khi request cuối cùng được thực thi
@@ -143,9 +143,30 @@ namespace Shopping_Project.Controllers
             // requets nào được thực hiện thì session sẽ bị xóa
             // Nếu trước thời điểm timeout, có 1 requets nào đó được thực thi
             // thì bộ đếm sẽ được reset
+            // Khi muốn chủ động xóa Session ta có 2 cách
+            // 1. Xóa tất
+            // HttpContext.Session.Clear();
+            // 2. Xóa theo key cụ thể
+            // HttpContext.Session.Remove("thongbao");
+            return View();
+            // Thử truyền 1 list đối tượng vào Session sau đó đọc lại
+        }
 
+        public IActionResult AddToCart(Guid id)
+        {
+            // Các bước thực hiện thêm giỏ hàng với session
+            // Bước 1: Xác nhận sản phẩm theo ID
+            var product = productServices.GetProductById(id);
+            // Bước 2: Lấy ra danh sách các SP trong giỏ hàng
+
+            // Bước 3: Sau khi xác nhận thêm / Ghi đè vào session
+            return RedirectToAction("ShowCart");
+        }
+        public IActionResult ShowCart()
+        {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Shopping_Project/Services/SessionServices.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Shopping_Project.Models;
+
+namespace Shopping_Project.Services
+{
+    public static class SessionServices
+    {
+        // Lấy ra danh sách các SP từ session
+        public static List<Product> GetObjFromSession(ISession session, string key)
+        {
+            string jsonData = session.GetString(key); // Lấy data dạng string từ Session
+            if(jsonData == null ) // Chưa có gì trong Session
+            {
+                return new List<Product>(); // Trả về 1 danh sách sản phẩm rỗng
+            }
+            else{
+                // Nếu dữ liệu có thì ta chuyển đổi dữ liệu thu được về dạng List<Obj>
+                var products = JsonConvert.DeserializeObject<List<Product>>(jsonData);
+                return products;
+            }
+        }
+        // Ghi lại danh sách SP vào session
+        public static void SetObjToSession(ISession session, string key, object data)
+        {
+            // Chuyển đổi dữ liệu ban đầu về dạng chuỗi Json để ghi vào session
+            var jsonData = JsonConvert.SerializeObject(data);
+            session.SetString(key, jsonData);   
+        }
+        // Kiểm tra xem SP có nằm trong 1 List hay không
+        public static bool CheckObjInList(Guid id, List<Product> products)
+        {
+            return products.Any(p => p.Id == id);
+            // Trả về true nếu điều kiện thỏa mãn và list không rỗng, false nếu ngược lại
+        }
+    }
+}
